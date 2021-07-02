76e5511ea472e9299fb4362d88d2d642b513be7b
Anshul24gupta <85728850+Anshul24gupta@users.noreply.github.com>
2021-07-02 13:31:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var boy ,path ,leftbound , rightboundry  , pathimg, boyimg, i; 
+function preload(){
+ pathimg=loadImage("path.png");
+ boyimg=loadImage("Runner-1.png","Runner-2.png") 
+  //pre-load images
+}
+
+function setup(){
+  createCanvas(400,400);
+  path=createSprite(200,200);
+  path.addImage(pathimg);
+  path.velocityY = 4; path.scale=1.2;
+  boy = createSprite(180,340,30,30); boy.scale=0.08;
+  boy.addImage(boyimg);
+  boy.addImage(boyimg);
+  leftbound=createSprite(0,0,100,800); leftbound.visible = false;
+  rightboundry=createSprite(410,0,100,800); rightboundry.visible = false;
+  
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  boy.x = World.mouseX; 
+  edges= createEdgeSprites();
+  boy.collide(edges[3]); boy.collide(leftbound); boy.collide(rightboundry);
+  if(path.y > 400 ){ path.y = height/2; } 
+  drawSprites()
+
+  }
+
